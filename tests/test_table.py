148f8656c6a8cc481fa3b6e299e40ca92efb5efb
Omar Sosa <omarfsosa@gmail.com>
2022-01-25 11:25:17 +0000

    more test cases for table

--- tests/test_table.py
+++ tests/test_table.py
@@ -8,8 +8,34 @@ from phil.table import LookupTable
 def table():
     return LookupTable()
 
+def known_hands():
+    return {
+        "argnames": ("string", "ranking"),
+        "argvalues": [
+            ("ah kh qh jh th", 1),
+            ("ah ad as ac kh", 11),
+            ("ah ad as kc kh", 167),
+            ("ah kh qh jh 9h", 323),
+            ("ah kh qh jh td", 1600),
+            ("ah ad as kc qh", 1610),
+            ("ah ad ks kc qh", 2468),
+            ("ah ad ks qc jh", 3326),
+            ("ah kh qh jh 9s", 6186),
+        ],
+        "ids": [
+            "Straight flush",
+            "Four of a kind",
+            "Full house",
+            "Flush",
+            "Straight",
+            "Three of a kind",
+            "Two pair",
+            "One pair",
+            "High card",
+        ]
+    }
 
-@pytest.mark.parametrize("hand", [("ah kh qh jh th", 1), ("2s 3h 4c 5d 7h", 7462)])
-def test_table(table, hand):
-    string, ranking = hand
+
+@pytest.mark.parametrize(**known_hands())
+def test_table(table, string, ranking):
     assert table.find(Hand.from_string(string)) == ranking
